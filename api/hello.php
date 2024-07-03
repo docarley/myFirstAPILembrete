4269a8cb6dbd16250c5161f74b8e562fc5cd4866
Arley <docente.arley@gmail.com>
2024-07-03 12:37:01 -0300

    alterações

--- api/hello.php
+++ api/hello.php
@@ -1,15 +1,19 @@
 <?php 
-require ('../config.php');
+$db_servidor = 'localhost';
+$db_banco = 'myfirstapilembretedb';
+$db_usuario = 'docarley';
+$db_pwd = 'senac123';
+
+//um site nao pode fazer requisicao a outro site sem a devida permissão - a api deve permitir que determinadas pessoas acessem ou geral
+header("Access-Control-Allow-Origin:*"); //Para domínio específico só colocar o nome no lugar do * que é aberta a todos
+header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS"); //Para definir quais metodos podem ser enviados para a api o options e executado sempre antes do get nos bastidores.
+header("Content-Type: application/json"); //Informar o tipo de retorno Devemos pois apps sem esta informação apresentarao erros o navegador até se vira bem com isso
+
+$pdo = new PDO ("mysql:host=$db_servidor;dbname=$db_banco,$db_usuario,$db_pwd");
 
 $array=[];
 
-$array['hello']='world';
-
-//um site nao pode fazer requisicao a outro site sem a devida permissão - a api deve permitir que determinadas pessoas acessem ou geral
-
-header("Access-Control-Allow-Origin: *"); //Para domínio específico só colocar o nome no lugar do * que é aberta a todos
-header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS"); //Para definir quais metodos podem ser enviados para a api o options e executado sempre antes do get nos bastidores.
-header("Content-Typ: application/json"); //Informar o tipo de retorno Devemos pois apps sem esta informação apresentarao erros o navegador até se vira bem com isso
+$array['hello']=true;
 
 echo json_encode($array);
 exit;
--- return.php
+++ return.php
@@ -1,14 +1,8 @@
-<?php   
-$db_servidor = 'localhost';
-$db_banco = 'myfirstapilembretedb';
-$db_usuario = 'docarley';
-$db_pwd = 'senac123';
-
+<?php
 //um site nao pode fazer requisicao a outro site sem a devida permissão - a api deve permitir que determinadas pessoas acessem ou geral
-header("Access-Control-Allow-Origin:*"); //Para domínio específico só colocar o nome no lugar do * que é aberta a todos
+header("Access-Control-Allow-Origin: *"); //Para domínio específico só colocar o nome no lugar do * que é aberta a todos
 header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS"); //Para definir quais metodos podem ser enviados para a api o options e executado sempre antes do get nos bastidores.
 header("Content-Type: application/json"); //Informar o tipo de retorno Devemos pois apps sem esta informação apresentarao erros o navegador até se vira bem com isso
 
-$pdo = new PDO ("mysql:dbname=$db_banco;host=$db_servidor,$db_usuario,$db_pwd");
-
-$array=[];
+echo json_encode($array);
+exit;
