1dc38db4e38a0022aa2abb0cde110db8078ef225
Arley <docente.arley@gmail.com>
2024-07-03 14:14:19 -0300

    Funcionando normal - corrigido parametros do pdo

--- api/hello.php
+++ api/hello.php
@@ -1,19 +1,19 @@
 <?php 
-$db_servidor = 'localhost';
-$db_banco = 'myfirstapilembretedb';
-$db_usuario = 'docarley';
-$db_pwd = 'senac123';
 
-//um site nao pode fazer requisicao a outro site sem a devida permissão - a api deve permitir que determinadas pessoas acessem ou geral
-header("Access-Control-Allow-Origin:*"); //Para domínio específico só colocar o nome no lugar do * que é aberta a todos
-header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS"); //Para definir quais metodos podem ser enviados para a api o options e executado sempre antes do get nos bastidores.
-header("Content-Type: application/json"); //Informar o tipo de retorno Devemos pois apps sem esta informação apresentarao erros o navegador até se vira bem com isso
+require('../config.php');
 
-$pdo = new PDO ("mysql:host=$db_servidor;dbname=$db_banco,$db_usuario,$db_pwd");
+// //um site nao pode fazer requisicao a outro site sem a devida permissão - a api deve permitir que determinadas pessoas acessem ou geral
+// header("Access-Control-Allow-Origin:*"); //Para domínio específico só colocar o nome no lugar do * que é aberta a todos
+// header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, OPTIONS"); //Para definir quais metodos podem ser enviados para a api o options e executado sempre antes do get nos bastidores.
+// header("Content-Type: application/json"); //Informar o tipo de retorno Devemos pois apps sem esta informação apresentarao erros o navegador até se vira bem com isso
 
-$array=[];
+// $pdo = new PDO ('mysql:host='.$db_servidor.';dbname='.$db_banco,$db_usuario,$db_pwd);
 
-$array['hello']=true;
+// $array=[];
 
-echo json_encode($array);
-exit;
+$array['hello']='world';
+
+require('../return.php');
+
+// echo json_encode($array);
+// exit;
